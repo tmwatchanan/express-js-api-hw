298c6c04a853c82408f73aba5ef6c9813479e0da
tmwatchanan <tmwatchanan@gmail.com>
2017-11-18 03:16:32 +0700

    Change port

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'port': 3000,
+    'port': 8080,
     'hostname': 'localhost',
     'secret': 'youcannotknowtommyssecretkey',
     'database': 'mongodb://tommy:tommy@ds243335.mlab.com:43335/cpe405'
